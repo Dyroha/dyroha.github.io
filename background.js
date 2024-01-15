93447912d266ba84e7164c90158fe82f0ef9c585
Dylan Hamilton <drh32@kent.ac.uk>
2024-01-15 21:48:36 +0000

    first steps to background

---
+++ background.js
@@ -0,0 +1,5 @@
+const triangle = "<div class='triangle'><div class='tpart ttop'></div><div class='tpart tleft'></div><div class='tpart tright'></div></div>";
+
+let width = innerWidth;
+let height = innerHeight;
+
